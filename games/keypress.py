580cf073e4fb09a4be1085ee34476b9ac8ca933e
Matthias Bolte <matthias@tinkerforge.com>
2013-12-06 18:43:54 +0100

    Unify keypress code

--- games/keypress.py
+++ games/keypress.py
@@ -16,35 +16,34 @@ from repeated_timer import RepeatedTimer
 import config
 
 class MultiTouchInput:
-    UID = config.UID_MULTI_TOUCH_BRICKLET
-
     current_state = 0
     current_state_counter = [0]*12
 
     def __init__(self, ipcon, key_queue):
-        if self.UID == None:
+        if not config.UID_MULTI_TOUCH_BRICKLET:
             print("Not Configured: Multi Touch")
-            
+            return
+
         self.key_queue = key_queue
         self.ipcon = ipcon
-        self.mt = MultiTouch(self.UID, self.ipcon)
+        self.mt = MultiTouch(config.UID_MULTI_TOUCH_BRICKLET, self.ipcon)
+
         try:
             self.mt.get_electrode_sensitivity()
-            print("Found: Multi Touch ({0})").format(self.UID)
+            print("Found: Multi Touch ({0})").format(config.UID_MULTI_TOUCH_BRICKLET)
         except:
-            print("Not Found: Multi Touch ({0})").format(self.UID)
-            self.UID = None
+            print("Not Found: Multi Touch ({0})").format(config.UID_MULTI_TOUCH_BRICKLET)
             return
 
         self.mt.register_callback(self.mt.CALLBACK_TOUCH_STATE, self.cb_touch_state)
-        
+
         self.touch_timer = RepeatedTimer(0.1, self.touch_tick)
 
     def state_to_queue(self, state):
         for item in config.KEYMAP_MULTI_TOUCH.items():
             if state & (1 << item[0]):
                 self.key_queue.put(item[1])
-    
+
     def touch_tick(self):
         state = 0
         for i in range(12):
@@ -68,50 +67,50 @@ class MultiTouchInput:
         return self.key_queue.get()
 
 class DualButtonInput:
-    UID = config.UID_DUAL_BUTTON_BRICKLET
-
     current_state = 0
     current_state_counter = [0]*4
 
     def __init__(self, ipcon, key_queue):
-        if self.UID[0] == None:
+        if not config.UID_DUAL_BUTTON_BRICKLET[0]:
             print("Not Configured: Dual Button 1")
-        if self.UID[1] == None:
-            print("Not Configured: Dual Button 2")
-            
-        self.key_queue = key_queue
 
+        if not config.UID_DUAL_BUTTON_BRICKLET[1]:
+            print("Not Configured: Dual Button 2")
+
+        self.key_queue = key_queue
         self.ipcon = ipcon
-        
-        if self.UID[0] != None:
-            self.db1 = DualButton(self.UID[0], self.ipcon)
+
+        if config.UID_DUAL_BUTTON_BRICKLET[0]:
+            self.db1 = DualButton(config.UID_DUAL_BUTTON_BRICKLET[0], self.ipcon)
         else:
             self.db1 = None
-            
-        if self.UID[1] != None:
-            self.db2 = DualButton(self.UID[1], self.ipcon)
+
+        if config.UID_DUAL_BUTTON_BRICKLET[1]:
+            self.db2 = DualButton(config.UID_DUAL_BUTTON_BRICKLET[1], self.ipcon)
         else:
             self.db2 = None
-        
-        try:
-            self.db1.get_button_state()
-            print("Found: Dual Button 1 ({0})").format(self.UID[0])
-        except:
-            print("Not Found: Dual Button 1 ({0})").format(self.UID[0])
-            self.UID = (None, self.UID[1])
-        
-        try:
-            self.db2.get_button_state()
-            print("Found: Dual Button 2 ({0})").format(self.UID[1])
-        except:
-            print("Not Found: Dual Button 2 ({0})").format(self.UID[1])
-            self.UID = (self.UID[0], None)
 
-        if self.db1 != None:
+        if self.db1:
+            try:
+                self.db1.get_button_state()
+                print("Found: Dual Button 1 ({0})").format(config.UID_DUAL_BUTTON_BRICKLET[0])
+            except:
+                self.db1 = None
+                print("Not Found: Dual Button 1 ({0})").format(config.UID_DUAL_BUTTON_BRICKLET[0])
+
+        if self.db2:
+            try:
+                self.db2.get_button_state()
+                print("Found: Dual Button 2 ({0})").format(config.UID_DUAL_BUTTON_BRICKLET[1])
+            except:
+                self.db2 = None
+                print("Not Found: Dual Button 2 ({0})").format(config.UID_DUAL_BUTTON_BRICKLET[1])
+
+        if self.db1:
             self.db1.register_callback(self.db1.CALLBACK_STATE_CHANGED, self.cb_state_changed1)
-        if self.db2 != None:
+        if self.db2:
             self.db2.register_callback(self.db2.CALLBACK_STATE_CHANGED, self.cb_state_changed2)
-        
+
         self.press_timer = RepeatedTimer(0.1, self.press_tick)
 
     def cb_state_changed1(self, button_l, button_r, led_l, led_r):
@@ -138,7 +137,7 @@ class DualButtonInput:
         for item in config.KEYMAP_DUAL_BUTTON.items():
             if state & (1 << item[0]):
                 self.key_queue.put(item[1])
-    
+
     def press_tick(self):
         state = 0
         for i in range(4):
@@ -164,8 +163,9 @@ class KeyBoardInput:
         self.prepare_stdin()
 
         self.thread = Thread(target = self.keyboard_loop)
+        self.thread.daemon = True
         self.thread.start()
-    
+
     def prepare_stdin(self):
         # save old state
         self.flags_save = fcntl.fcntl(self.fd, fcntl.F_GETFL)
@@ -175,8 +175,8 @@ class KeyBoardInput:
         attrs = list(self.attrs_save)
 
         # iflag
-        attrs[0] &= ~(termios.IGNBRK | termios.BRKINT | termios.PARMRK 
-                      | termios.ISTRIP | termios.INLCR | termios. IGNCR 
+        attrs[0] &= ~(termios.IGNBRK | termios.BRKINT | termios.PARMRK
+                      | termios.ISTRIP | termios.INLCR | termios. IGNCR
                       | termios.ICRNL | termios.IXON )
 
         # cflag
@@ -200,8 +200,8 @@ class KeyBoardInput:
         while self.loop:
             try:
                 self.key_queue.put(sys.stdin.read(1)) # read single character
-            except KeyboardInterrupt: 
-                pass 
+            except KeyboardInterrupt:
+                pass
 
 class KeyPress:
     key_queue = Queue()
