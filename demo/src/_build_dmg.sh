999ff10f517ba8f7a7753ad5a667fc5a75e37bef
Matthias Bolte <matthias@tinkerforge.com>
2014-02-10 18:53:26 +0100

    demo: Fix Mac OS X package

--- demo/src/_build_dmg.sh
+++ demo/src/_build_dmg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 dot_version=`grep DEMO_VERSION config.py | sed -e 's/DEMO_VERSION = "\(.*\)"/\1/'`
 underscore_version=`printf %s $dot_version | sed -e 's/\./_/g'`
-dmg=starter_kit_weather_station_demo_macos_$underscore_version.dmg
+dmg=starter_kit_blinkenlights_demo_macos_$underscore_version.dmg
 rm $dmg
 hdiutil create -fs HFS+ -volname "Starter-Kit-Blinkenlights-Demo-$dot_version" -srcfolder dist $dmg
